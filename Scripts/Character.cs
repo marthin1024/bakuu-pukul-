4dbe86d23eeaa967ee64e958131f9f4731262704
marthin1024 <marthinmiracle417@gmail.com>
2025-08-24 23:48:47 +0800

    Add basic Character script

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,1 +1,27 @@
-Placeholder Character Script
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Gerakan kiri/kanan
+        float move = Input.GetAxis("Horizontal") * moveSpeed;
+        rb.velocity = new Vector3(move, rb.velocity.y, 0);
+
+        // Lompat
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
